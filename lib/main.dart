dc0c4f7b150216bd5f4f5f02752d02899f2d7c82
michelle <hwrng2@gmail.com>
2023-02-19 01:10:55 +0900

    feat: http request & WebtoonModel 생성

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:pmfm/screens/pomodoros_screen.dart';
+import 'package:pmfm/screens/home_screem.dart';
+import 'package:pmfm/services/api_service.dart';
 
 void main() {
+  ApiService().getTodaysToons();
   runApp(const App());
 }
 
-class App extends StatefulWidget {
+class App extends StatelessWidget {
   const App({super.key});
 
   @override
-  State<App> createState() => _AppState();
-}
-
-class _AppState extends State<App> {
-  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        theme: ThemeData(
-          textTheme: const TextTheme(
-            displayLarge: TextStyle(
-              color: Color(0xFF232B55),
-            ),
-          ),
-          colorScheme: ColorScheme.fromSwatch(
-            backgroundColor: const Color(0xFFE7626C),
-          ),
-          cardColor: const Color(0xFFF3EDDB),
-        ),
-        home: const PhomodorosScreen());
-  }
-}
-
-class MyLargeTitle extends StatelessWidget {
-  const MyLargeTitle({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      'My Large Title',
-      style: TextStyle(
-        fontSize: 30,
-        color: Theme.of(context).textTheme.titleLarge?.color,
-      ),
-    );
+    return const MaterialApp(home: HomeScreen());
   }
 }
---
+++ lib/services/api_service.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:pmfm/models/webtoon_model.dart';
+
+class ApiService {
+  final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
+  final String today = "today";
+
+  Future<List<WebtoonModel>> getTodaysToons() async {
+    List<WebtoonModel> webtoonInstances = [];
+    final url = Uri.parse('$baseUrl/$today');
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final List<dynamic> webtoons = jsonDecode(response.body);
+      for (var webtoon in webtoons) {
+        final toon = WebtoonModel.fromJson(webtoon);
+        webtoonInstances.add(toon);
+      }
+      print(webtoonInstances);
+      return webtoonInstances;
+    }
+
+    throw Error();
+  }
+}
