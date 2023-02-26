544e79d1c5e3836268696a10473c38e8362b7996
michelle <hwrng2@gmail.com>
2023-02-26 18:08:20 +0900

    feat: tap change 기능 추가

--- lib/screens/plan_screen.dart
+++ lib/screens/plan_screen.dart
@@ -27,43 +27,60 @@ class PlanScreen extends StatelessWidget {
   }
 }
 
-class BottomNavigatorBar extends StatelessWidget {
+class BottomNavigatorBar extends StatefulWidget {
   const BottomNavigatorBar({
     super.key,
   });
-  // BottomNavigationBar
+
+  @override
+  State<BottomNavigatorBar> createState() => _BottomNavigatorBarState();
+}
+
+class _BottomNavigatorBarState extends State<BottomNavigatorBar> {
+  int selectedTabIndex = 0;
+
+  void changeTap(int idx) {
+    setState(() {
+      selectedTabIndex = idx;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-        showUnselectedLabels: true,
-        currentIndex: 0,
-        selectedItemColor: Colors.lime,
-        unselectedItemColor: Colors.brown.shade400,
-        items: const [
-          BottomNavigationBarItem(
-              icon: Icon(
-                Icons.eco_rounded,
-                size: 28,
-              ),
-              label: "오늘의 잎"),
-          BottomNavigationBarItem(
-              icon: Icon(
-                Icons.park_rounded,
-                size: 28,
-              ),
-              label: "2월의 숲"),
-          BottomNavigationBarItem(
-              icon: Icon(
-                Icons.water_drop,
-                size: 28,
-              ),
-              label: "같이 자라기"),
-          BottomNavigationBarItem(
-              icon: Icon(
-                Icons.settings,
-                size: 28,
-              ),
-              label: "설정")
-        ]);
+    return Theme(
+        data: ThemeData(canvasColor: Colors.white),
+        child: BottomNavigationBar(
+          showUnselectedLabels: true,
+          currentIndex: selectedTabIndex,
+          onTap: (value) => changeTap(value),
+          selectedItemColor: Colors.lime,
+          unselectedItemColor: Colors.brown.shade400,
+          items: const [
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.eco_rounded,
+                  size: 28,
+                ),
+                label: "오늘의 잎"),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.park_rounded,
+                  size: 28,
+                ),
+                label: "2월의 숲"),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.water_drop,
+                  size: 28,
+                ),
+                label: "같이 자라기"),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.settings,
+                  size: 28,
+                ),
+                label: "설정")
+          ],
+        ));
   }
 }
