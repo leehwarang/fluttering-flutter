55c99c90ab58da5c0d87bddfe77336818fe84bc8
michelle <hwrng2@gmail.com>
2023-03-04 02:42:49 +0900

    feat: home 화면 러프하게 잡기

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,18 +7,77 @@ class HomeScreen extends StatelessWidget {
   HomeScreen({super.key});
   late BottomNavigationBarProvider _bottomNavigationBarProvider;
 
+  // 날짜를 어딘가에서 보여주긴해야 할 듯
+
   @override
   Widget build(BuildContext context) {
     _bottomNavigationBarProvider =
         Provider.of<BottomNavigationBarProvider>(context);
 
+    const String nickname = "화랑";
+    final String appBartitle = _bottomNavigationBarProvider.leafType == "today"
+        ? '$nickname님, 오늘은'
+        : '$nickname님, 내일은';
+
+    final String emptyContentText =
+        _bottomNavigationBarProvider.leafType == "today"
+            ? '어제 세워둔 계획이 없어요'
+            : '아직 세워둔 계획이 없어요';
+
     return Scaffold(
       backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        centerTitle: false,
+        title: Text(
+          appBartitle,
+          style: const TextStyle(
+            color: Colors.lime,
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () => {},
+            splashColor: Colors.transparent,
+            highlightColor: Colors.transparent,
+            icon: const Icon(
+              Icons.space_dashboard_sharp,
+              color: Colors.lime,
+            ),
+          )
+        ],
+      ),
       body: SafeArea(
-        child: Column(
-          children: [
-            Text(_bottomNavigationBarProvider.leafType),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: 5,
+            horizontal: 20,
+          ),
+          child: Column(
+            children: [
+              Row(
+                children: const [
+                  Text(
+                    "3월 4일\n토요일",
+                    style: TextStyle(
+                      fontSize: 28,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                ],
+              ),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(emptyContentText),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
